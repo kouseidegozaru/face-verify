0ffe703c4efdbe47340577b17c8eccfe9f6bfc1f
kouseidegozaru <gguangchui@gmail.com>
2024-07-17 09:30:33 +0900

    ユーザーモデルの作成

--- backend_app/api_project/accounts/models.py
+++ backend_app/api_project/accounts/models.py
@@ -1,3 +1,52 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, name, password):
+        if not email:
+            raise ValueError('ユーザーにはメールアドレスが必要です')
+        
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name,
+        )
+        
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, password):
+        user = self.create_user(
+            email=email,
+            name=name,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser):
+    email = models.EmailField(verbose_name='メールアドレス', max_length=255, unique=True)
+    name = models.CharField(verbose_name='名前', max_length=100)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
